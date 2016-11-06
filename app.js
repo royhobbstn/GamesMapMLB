22dbddd6fe6d2a3619130d21d2286aa2862022ae
royhobbstn <royhobbstn@yahoo.com>
2016-11-06 16:54:09 -0500

    not clear why i need to alter paths with express static on proxy path

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 
 var express = require("express");
 var app = express();
+var path = require('path');
 
 app.use(express.static('public'));
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
