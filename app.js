4de4ed0a010013cd70c4a626e8535760e46be564
Daniel Trone <royhobbstn@yahoo.com>
2017-01-28 15:28:14 +0000

    change ports and paths around

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var path = require('path');
 app.use(express.static('public'));
 
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
- 
+
 var routes = require("./routes/routes.js")(app);
 
-var server = app.listen(4000, function () {
-    console.log("Listening on port %s...", server.address().port);
+var server = app.listen(8080, function () {
+  console.log("Listening on port %s...", server.address().port);
 });
