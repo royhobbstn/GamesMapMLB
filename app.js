c54fc4d410270d34da99041583d8267f33b97c36
Daniel Trone <royhobbstn@yahoo.com>
2017-02-05 16:01:43 +0000

    this will work

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
 // Adapted from: http://itblog.mobi/2015/12/29/how-to-create-a-simple-restful-api-with-node-js/
 
-console.log(process.env.NODE_ENV);
+
 
 var express = require("express");
 var app = express();
 
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  if (req.url.slice(-4) === '/mlb')
+    res.redirect(301, req.url + '/');
+  else
+    next();
 });
 
-// app.use(function (req, res, next) {
-//   if (req.url.slice(-1) !== '/' && req.url.slice(-3) !== '.js' && req.url.slice(-3) !== 'tml' && req.url.slice(-3) !== 'css')
-//     res.redirect(301, req.url + '/');
-//   else
-//     next();
-// });
 
-var folder_path = '';
+app.use(express.static('public'));
 
-if (process.env.NODE_ENV === 'docker') {
-  folder_path = 'public';
-}
-else {
-  folder_path = 'public/mlb';
-}
-
-app.use(express.static(folder_path));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
