ec8484d3ad494ddf45f2ec5ee68df52f3b57f5d9
Daniel Trone <royhobbstn@yahoo.com>
2017-02-05 16:47:33 +0000

    just server

--- app.js
+++ app.js
@@ -6,25 +6,6 @@ var express = require("express");
 var app = express();
 
 
-
-app.use(function (req, res, next) {
-  console.log(req.url);
-  console.log(req.url.slice(-3))
-  console.log(req.originalUrl);
-  console.log(req.originalUrl.slice(-3));
-
-  // if (req.url.slice(-3) === '/') {
-  //   console.log('this');
-  //   res.redirect(301, req.url + 'mlb/');
-  // }
-  // else
-  next();
-});
-
-
-app.use(express.static('public'));
-
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
