e637007ae96a8b714f323b52e90dd91694acd937
Daniel Trone <royhobbstn@yahoo.com>
2017-02-05 12:32:39 +0000

    env variables

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Adapted from: http://itblog.mobi/2015/12/29/how-to-create-a-simple-restful-api-with-node-js/
 
+console.log(process.env.NODE_ENV);
+
 var express = require("express");
 var app = express();
 var path = require('path');
@@ -7,8 +9,13 @@ var path = require('path');
 // app.all(/^\/main$/, function(req, res) { res.redirect('/main/'); });
 // app.use('/main/',express.static(__dirname+'/public'));
 
-app.use(express.static(path.join(__dirname, '/./public')));
+var prod_path = '';
 
+if (process.env.NODE_ENV === 'docker') {
+  prod_path = '/mlb/;'
+}
+
+app.use(express.static(path.join(prod_path, 'public')));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
